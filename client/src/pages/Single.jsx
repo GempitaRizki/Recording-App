862a63a821d95a05562fd455362994866d2613b3
Gempita Rizki (R) ? <38187462+GempitaRizki@users.noreply.github.com>
2022-09-30 20:46:44 +0700

    20:46

--- client/src/pages/Single.jsx
+++ client/src/pages/Single.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const Single = () => {
   return (
-    <div>Single</div>
+    <div className='single'>
+    <div className='content'>
+      <img src='https://www.xtrafondos.com/wallpapers/vertical/el-rayo-de-thor-7655.jpg' alt='' />
+    </div>
+    <div className='menu'>m</div>
+    </div>
   )
 }
 
