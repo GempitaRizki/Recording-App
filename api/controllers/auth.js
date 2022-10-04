42f328f5960faa37a4c313f75e2419bb3f25d600
Gempita Rizki (R) ? <38187462+GempitaRizki@users.noreply.github.com>
2022-10-04 13:04:45 +0700

    Api

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -1,5 +1,14 @@
+import {db} from "../db.js"
+
 export const register = (req,res) =>{
-    
+    // CHECK APAKAH USER SUDAH ADA 
+
+    const q = "SELECT * FROM users WHERE email = ? OR username = ?"
+
+    db.query(q,[req.body.email , req.body.username], (err,data) => {
+        if(err) return res.json(err)
+        if(data.length) return res.status(409).json("User sudah ada!")
+    })
 }
 export const login = (req,res) =>{
     
