fc8395848fa7746b8d69178b5100e70540abd6ba
Gempita Rizki (R) ? <38187462+GempitaRizki@users.noreply.github.com>
2022-10-04 22:09:46 +0700

    ECONNREFUSED ERR

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -1,4 +1,5 @@
 import {db} from "../db.js"
+import bcrypt from "bcryptjs"
 
 export const register = (req,res) =>{
     // CHECK APAKAH USER SUDAH ADA 
@@ -8,12 +9,24 @@ export const register = (req,res) =>{
     db.query(q,[req.body.email , req.body.username], (err,data) => {
         if(err) return res.json(err)
         if(data.length) return res.status(409).json("User sudah ada!")
+    
+        //hash the password and create a user(bcrypt reason)
+        const salt = bcrypt.genSaltSync(10);
+        const hash = bcrypt.hashSync(req.body.password, salt);
+
+        
+        const q = "INSERT INTO users(`username`,`email`,`password`) VALUES (?)"
+        const values =  [
+            req.body.username,
+            req.body.email,
+            hash,
+        ]
+
+        db.query(q[values] , (err,data) => {
+            if (err) return res.json(err)
+        return res.status(200).json('User berhasil dibuat')
+        })
     })
 }
-export const login = (req,res) =>{
-    
-}
-
-export const logout = (req,res) =>{
-    
-}
+export const login = (req,res) =>{}
+export const logout = (req,res) =>{}
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,15 +1,38 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { useState } from 'react'
+import axios from 'axios'
 
 const Register = () => {
+  const [inputs, setInputs] = useState({
+    username:"",
+    email:"",
+    password:""
+
+  })
+
+  const handleChange = e => {
+    setInputs(prev=>({...prev, [e.target.username]: e.target.value}))
+  }
+
+const handleSubmit = async e => {
+  e.preventDefault()
+  try{
+
+  const res = await axios.post("/auth/register", inputs)
+  console.log(res)
+}catch(err){
+  console.log(err)
+  }
+}
   return (
     <div className='auth'>
       <h1>Register</h1>
       <form>
-        <input required type="text" placeholder='username'/>
-        <input required type="email" placeholder='email'/>
-        <input required type="password" placeholder='password' />        
-        <button>Daftar</button>
+        <input required type="text" placeholder='username' name='username' onChange={handleChange}/>
+        <input required type="email" placeholder='email'name='email' onChange={handleChange}/>
+        <input required type="password" placeholder='password' name='password' onChange={handleChange}/>        
+        <button onClick={handleSubmit}>Daftar</button>
         <p>Error!</p>
         <span>Apakah kamu memiliki akun? <Link to="/login">Login</Link>
         </span>
