3d2078f2395f8cd4738dc78f584985d1903eb61a
Gempita Rizki (R) ? <38187462+GempitaRizki@users.noreply.github.com>
2022-10-06 13:10:34 +0700

    .

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -12,7 +12,7 @@ const Register = () => {
   })
 
   const handleChange = e => {
-    setInputs(prev=>({...prev, [e.target.username]: e.target.value}))
+    setInputs(prev=>({...prev, [e.target.name]: e.target.value}))
   }
 
 const handleSubmit = async e => {
